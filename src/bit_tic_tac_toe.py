ffad24303772f0e8cdca6db7745fece949bae04a
Reid Goodbar <reidgoodbar@uchicago.edu>
2022-01-02 01:07:58 -0500

    fixed thing

--- src/bit_tic_tac_toe.py
+++ src/bit_tic_tac_toe.py
@@ -1,14 +1,22 @@
-# Reid Goodbar
-# Tic Tac Toe using bit string to store and play, allows for single player and multi-player tic tac toe
-# board is an int with each space being two bits, top left is the first two bits and the bottom right is the 17th and 18th bits
-# move: flag for whose move it is, 1 for player 1 and 2 for player 2
+"""
+    Reid Goodbar
+
+    Tic Tac Toe using bit string to store and play, allows for single
+    player and multi-player tic tac toe, board is an int with each space
+    being two bits, top left is the first two bits and the bottom right
+    is the 17th and 18th bits
+    move: flag for whose move it is, 1 for player 1 and 2 for player 2
+"""
 
 import random
 
-# game: plays the game, alternates back and forth between player 1 and player 2
+
 def game():
+    """
+        game: plays the game, alternates back and forth between player 1 and player 2
+    """
     results = {}
-    for i in range(1000):
+    for _ in range(1000):
         print("New Game")
         board = 0
         move = 1
@@ -43,8 +51,10 @@ def game():
         print("% d : % d" % (key, value))
 
 
-# input_move: takes in users move, determines if valid move
 def input_move(board, move):
+    """
+        input_move: takes in users move, determines if valid move
+    """
     inpt = int(input("Your Move: "))
     if inpt > 8 or inpt < 0 or (board >> (2 * inpt) & 3):
         print("illegal move")
@@ -54,8 +64,10 @@ def input_move(board, move):
     return board
 
 
-# random_move: computer performs a valid random move
 def random_move(board, move):
+    """
+        random_move: computer performs a valid random move
+    """
     rndm = random.randint(0, 9)
     if rndm > 8 or rndm < 0 or (board >> (2 * rndm) & 3):
         board = random_move(board, move)
@@ -64,67 +76,77 @@ def random_move(board, move):
     return board
 
 
-searched = {}
-# computer_move: computer finds best move to play
+SEARCHED = {}
+
+
 def computer_move(board, move):
-    global searched
+    """
+        computer_move: computer finds best move to play
+    """
+    global SEARCHED
     computer_move_search(board, move)
     move_delta = -1
     winning_move = random_move(board, move)
-    for i in searched:
+    for i in SEARCHED:
         move_count = 0
         other_count = 0
-        if move in searched[i]:
-            move_count = searched[i][move]
-        if move % 2 + 1 in searched[i]:
-            other_count = searched[i][move % 2 + 1]
+        if move in SEARCHED[i]:
+            move_count = SEARCHED[i][move]
+        if move % 2 + 1 in SEARCHED[i]:
+            other_count = SEARCHED[i][move % 2 + 1]
         if (move_count - other_count) > move_delta:
             move_delta = move_count - other_count
             winning_move = i
-    print(searched)
+    print(SEARCHED)
     board = board | (move << 2 * winning_move)
-    searched = {}
+    SEARCHED = {}
     return board
 
 
-# computer_move_search: computer fills results array with all possible moves
 def computer_move_search(board, move):
+    """
+        computer_move_search: computer fills results array with all possible moves
+    """
     for i in range(9):
         if (board >> i * 2 & 3) == 0:
             board = board | (move << 2 * i)
             game_res = game_over(board)
-            global searched
+            global SEARCHED
 
-            if i not in searched:
-                searched[i] = {}
+            if i not in SEARCHED:
+                SEARCHED[i] = {}
 
             if game_res not in [1, 2, 3]:
                 computer_move_search(board, move % 2 + 1)
             else:
-                sub_search = searched[i]
+                sub_search = SEARCHED[i]
                 if game_res in sub_search:
                     sub_search[game_res] += 1
                 else:
                     sub_search[game_res] = 1
 
 
-# print_board: prints the board from the bit string
 def print_board(board):
+    """
+        print_board: prints the board from the bit string
+    """
     for i in range(0, 3):
         row_str = ""
         for j in range(0, 3):
-            sq = board >> (6 * i + 2 * j) & 3
-            if sq == 1:
+            square = board >> (6 * i + 2 * j) & 3
+            if square == 1:
                 row_str += "X"
-            elif sq == 2:
+            elif square == 2:
                 row_str += "O"
             else:
                 row_str += "-"
         print(row_str)
 
 
-# check_rows: determines if any row is complete
 def check_rows(board):
+    """
+        check_rows: determines if any row is complete
+    """
     for j in [1, 2]:
         for i in range(0, 3):
             if (
@@ -135,8 +157,10 @@ def check_rows(board):
                 return j
 
 
-# check_cols: determines if any col is complete
 def check_cols(board):
+    """
+        check_cols: determines if any col is complete
+    """
     for j in [1, 2]:
         for i in range(0, 3):
             if (
@@ -147,8 +171,10 @@ def check_cols(board):
                 return j
 
 
-# check_diag: determines if any diag is complete:
 def check_diag(board):
+    """
+        check_diag: determines if any diag is complete
+    """
     for i in [1, 2]:
         if (
             ((board & 3) == i) and ((board >> 8 & 3) == i) and ((board >> 16 & 3) == i)
@@ -160,16 +186,20 @@ def check_diag(board):
             return i
 
 
-# check_tie: determines if board is in tie state
 def check_tie(board):
+    """
+        check_tie: determines if board is in tie state
+    """
     for i in range(0, 9):
         if board >> 2 * i & 3 == 0:
             return None
     return 3
 
 
-# game_over: determines if a bit string represents a game over
 def game_over(board):
+    """
+        game_over: determines if a bit string represents a game over
+    """
     return (
         check_rows(board) or check_cols(board) or check_diag(board) or check_tie(board)
     )
