cd8f88ae17a21b1b29853702302ae07e68ba99e2
Reid Goodbar <reidgoodbar@uchicago.edu>
2022-01-18 01:15:43 -0600

    have everything working correctly

--- src/bit_tic_tac_toe.py
+++ src/bit_tic_tac_toe.py
@@ -4,7 +4,7 @@
     Tic Tac Toe using bit string to store and play, allows for single
     player and multi-player tic tac toe, board is an int with each space
     being two bits, top left is the first two bits and the bottom right
-    is the 17th and 18th bits
+    is the 17th and 18th bits.  Uses minmax to find optimal move for computer.
     move: flag for whose move it is, 1 for player 1 and 2 for player 2
 """
 
@@ -16,7 +16,7 @@ def game():
         game: plays the game, alternates back and forth between player 1 and player 2
     """
     results = {}
-    for _ in range(1000):
+    for _ in range(10):
         print("New Game")
         board = 0
         move = 1
@@ -24,14 +24,14 @@ def game():
         while not game_over(board):
             if move == 1:
                 print("X Move")
-                board = input_move(board, move)
+                board = computer_move(board, move)
+                # board = input_move(board, move)
                 # board = random_move(board, move)
-                # board = computer_move(board, move)
             else:
                 print("O Move")
-                board = computer_move(board, move)
-                # board = random_move(board, move)
+                # board = computer_move(board, move)
                 # board = input_move(board, move)
+                board = random_move(board, move)
             move = move % 2 + 1
             print_board(board)
         winner_move = game_over(board)
@@ -82,65 +82,67 @@ def random_move(board, move):
 
 def computer_move(board, move):
     """
-        computer_move: computer finds best move to play
+        computer_move: computer finds best move to play using minmax
     """
 
     moves = computer_move_search(board, move, move, 0)
-    print(moves)
-    winning_move = moves.index(max(moves))
-
-    # winning_move = random_move(board, move)
-
-    # for i, val in searched.items():
-    #     move_count = 0
-    #     other_count = 0
-    #     if move in val:
-    #         move_count = val[move]
-    #     if move % 2 + 1 in val:
-    #         other_count = val[move % 2 + 1]
-    #     if (move_count - other_count) > move_delta:
-    #         move_delta = move_count - other_count
-    #         winning_move = i
-
-    # print(searched)
+    winning_move = moves.index(board_max(moves))
     board = board | (move << 2 * winning_move)
-    # searched = {}
     return board
 
 
 def computer_move_search(board, move, curr_move, depth):
     """
-        computer_move_search: computer fills results array with all possible moves
+        computer_move_search: uses recursion and minimax to fill array of moves
     """
-    
-    print("---------------")
-    print(curr_move)
+
     res = [0] * 9
     original_board = board
     for i in range(9):
         if ((board >> (i * 2)) & 3) == 0:
             board = board | (curr_move << 2 * i)
-            print_board(board)
             game_res = game_over(board)
             if game_res not in [1, 2, 3]:
                 if move == curr_move:
-                    res[i] = min(
+                    res[i] = board_min(
                         computer_move_search(board, move, (curr_move % 2 + 1), depth)
                     )
                 else:
-                    res[i] = max(
+                    res[i] = board_max(
                         computer_move_search(board, move, (curr_move % 2 + 1), depth)
                     )
             elif game_res == move:
                 res[i] = 10
-            elif game_res == move % 2 + 1:
+            elif game_res == (move % 2 + 1):
                 res[i] = -10
             else:
                 res[i] = 0
             board = original_board
-    print(res)
-    print("---------------")
+        else:
+            res[i] = "-"
     return res
+
+
+def board_max(vals):
+    """
+        board_max: return max of a list when contains strings and ints
+    """
+    max_val = -float("inf")
+    for val in vals:
+        if isinstance(val, int) and val > max_val:
+            max_val = val
+    return max_val
+
+
+def board_min(vals):
+    """
+        board_min: return min of a list when contains strings and ints
+    """
+    min_val = float("inf")
+    for val in vals:
+        if isinstance(val, int) and val < min_val:
+            min_val = val
+    return min_val
 
 
 def print_board(board):
